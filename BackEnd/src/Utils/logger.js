a82483e1ed42e44d8973512738c2264ee3bdda6b
NamalChandrasekara <85227162+NamalChandrasekara@users.noreply.github.com>
2023-07-22 14:06:34 +0530

    jsoncom2

---
+++ BackEnd/src/Utils/logger.js
@@ -0,0 +1,15 @@
+import pino from "pino";
+
+const logger =pino({
+    transport:{
+        target:"pino-pretty",
+        options:{
+            colorize :true,
+            translateTime : 'SYS:yyyy-mm-dd HH:MM:ss',
+            ignore:"pid,hostname",
+
+        }
+    },
+});
+//logger.info("Helloo..");
+export default logger;
--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,17 +1,22 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
+import {connect} from "./Utils/DatabaseConnection"
+import logger from "./Utils/logger";
 
-dotenv.config();
+
 const PORT = process.env.PORT || "8090";
 const app =  express();
 app.use(cors());
 app.use(express.json({limit:"20mb"}));
 
 app.get("/",(req,res,next)=>{
-    res.send("<h2> Saru Blue Saphire Hotel </h2>")
-})
+    res.send("<h2> Saru Blue Saphire Hotel </h2>");
+    next();
+});
 
 app.listen(PORT,()=> {
-    console.log('server is up and running on port', PORT);
+    logger.info("This is testing");
+    logger.info('server is up and running on port', PORT);
+    connect();
 })
